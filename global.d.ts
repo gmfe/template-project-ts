e87d1c7ec940c81b03214ac9f654188008d1e116
liyatang <liyatang@qq.com>
2020-08-10 19:43:32 +0800

    feat: add global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+interface Window {
+  requestIdleCallback(fn: () => any): void
+  /* eslint-disable */
+  __wxjs_environment: string
+  __PRODUCTION__: string
+  __VERSION__: string
+  __NAME__: string
+  __CLIENT_NAME__: string
+  __COMMIT__: string
+  __BRANCH__: string
+  /* eslint-enable */
+}
+
+declare const __PRODUCTION__ = 'true'
+declare const __VERSION__ = '1.0.0'
+declare const __NAME__ = 'projectx'
+declare const __CLIENT_NAME__ = 'GmProjectX'
+declare const __COMMIT__ = 'none'
+declare const __BRANCH__ = 'none'
